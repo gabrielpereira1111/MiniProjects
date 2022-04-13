6b568d7fea9cde4bbecfcd6f865d7c49e5eca824
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-04-13 17:16:43 -0300

    Add genre domain

---
+++ filmes_project/filmes_webApi/filmes_webApi/Domains/GeneroDomain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filmes_webApi.Domains
+{
+    public class GeneroDomain
+    {
+        public int idGenero { get; set; }
+        public string Nome { get; set; }
+    }
+}
