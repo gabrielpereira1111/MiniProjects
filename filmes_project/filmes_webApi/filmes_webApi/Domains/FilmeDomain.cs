f1e513b4fe707dd70162200e2465e5a143e0d2da
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-04-13 17:18:25 -0300

    Add movie domain

---
+++ filmes_project/filmes_webApi/filmes_webApi/Domains/FilmeDomain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filmes_webApi.Domains
+{
+    public class FilmeDomain
+    {
+        public int idFilme { get; set; }
+        public string Nome { get; set; }
+        public int idGenero { get; set; }
+    }
+}
