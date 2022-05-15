a0b0132780b67b5b5e8d0c8226e39d3cbe75fdef
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-15 16:11:29 -0300

    .

--- filmes_project/filmes_webApi/filmes_webApi/Interfaces/IGeneroDomain.cs
+++ filmes_project/filmes_webApi/filmes_webApi/Interfaces/IGeneroDomain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using filmes_webApi.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace filmes_webApi.Interfaces
 {
     interface IGeneroDomain
     {
+        void Create(GeneroDomain genero);
     }
 }
