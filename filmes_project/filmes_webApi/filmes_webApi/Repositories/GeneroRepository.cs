7dbbf442ed192f950beecc1afd2133b953dc2152
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-17 15:28:19 -0300

    GeneroRepository Added

---
+++ filmes_project/filmes_webApi/filmes_webApi/Repositories/GeneroRepository.cs
@@ -0,0 +1,42 @@
+﻿using filmes_webApi.Domains;
+using filmes_webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filmes_webApi.Repositories
+{
+    public class GeneroRepository : IGeneroRepository
+    {
+        public void Create(GeneroDomain genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GeneroDomain GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<GeneroDomain> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBody(GeneroDomain genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUrl(GeneroDomain genero, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
