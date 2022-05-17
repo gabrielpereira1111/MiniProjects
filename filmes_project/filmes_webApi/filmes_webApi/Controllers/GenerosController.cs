bb8cf5774774a3fe2f174f2782d77b7a2193e6c2
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-17 16:56:45 -0300

    .

---
+++ filmes_project/filmes_webApi/filmes_webApi/Controllers/GenerosController.cs
@@ -0,0 +1,31 @@
+﻿using filmes_webApi.Domains;
+using filmes_webApi.Interfaces;
+using filmes_webApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filmes_webApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerosController : ControllerBase
+    {
+        private IGeneroRepository _GeneroRepository { get; set; }
+        public GenerosController()
+        {
+            _GeneroRepository = new GeneroRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<GeneroDomain> listaGeneros = _GeneroRepository.ReadAll();
+            return Ok(listaGeneros);
+        }
+    }
+}
