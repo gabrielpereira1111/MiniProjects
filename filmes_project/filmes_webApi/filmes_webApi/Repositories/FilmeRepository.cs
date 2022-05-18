07d5ca636100e8dcb7c69fdfdc302d3c1142d306
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-18 13:18:57 -0300

    .

---
+++ filmes_project/filmes_webApi/filmes_webApi/Repositories/FilmeRepository.cs
@@ -0,0 +1,42 @@
+﻿using filmes_webApi.Domains;
+using filmes_webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filmes_webApi.Repositories
+{
+    public class FilmeRepository : IFilmeRepository
+    {
+        public void Create(FilmeDomain filme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FilmeDomain GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FilmeDomain> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(FilmeDomain filme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUrl(FilmeDomain filme, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
