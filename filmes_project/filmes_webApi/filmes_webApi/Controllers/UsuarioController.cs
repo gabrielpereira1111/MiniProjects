a8a8183ebf10ce46c3a8620eca68a36fe769bdd5
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-22 14:07:27 -0300

    Add Token

--- filmes_project/filmes_webApi/filmes_webApi/Controllers/UsuarioController.cs
+++ filmes_project/filmes_webApi/filmes_webApi/Controllers/UsuarioController.cs
@@ -4,6 +4,7 @@ using filmes_webApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -33,6 +34,7 @@ namespace filmes_webApi.Controllers
             {
                // return Ok(usuarioBuscado);
 
+               // Payload
                var claims = new[]
                {
                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.idUsuario.ToString()),
@@ -40,6 +42,30 @@ namespace filmes_webApi.Controllers
                    new Claim(ClaimTypes.Role, usuarioBuscado.Permissao.ToString()),
                    new Claim("Role", usuarioBuscado.Permissao.ToString())
                };
+
+                // Key
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("minha-chave-segura-filmes"));
+
+                // Signature
+                var signature = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                //Token
+                var myToken = new JwtSecurityToken(
+                        issuer: "filmes_webApi",
+                        audience: "filmes_webApi",
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(30),
+                        signingCredentials: signature
+                    );
+
+                return Ok(
+                        new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(myToken)
+                        }
+                    );
+
+                
             }
 
             return NotFound("Email e/ou senha incorretos!");
