99f8eb7ea14db44bfc2c2857511cd89d5d63ae57
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-24 16:10:27 -0300

    Adiciona uma interface de pessoa

---
+++ pessoas_project/pessoas_webApi/pessoas_webApi/Interfaces/IPessoaRepository.cs
@@ -0,0 +1,13 @@
+﻿using pessoas_webApi.Domains;
+
+namespace pessoas_webApi.Interfaces
+{
+    public interface IPessoaRepository
+    {
+        public void Create(Pessoa pessoa);
+        public List<Pessoa> ReadAll();
+        public Pessoa GetById(int id);
+        public void Update(Pessoa pessoa);
+        public void Delete(int id);
+    }
+}
