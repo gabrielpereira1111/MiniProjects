738de7a257ca3008e79359b03cc1af1baaaf6020
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-24 17:04:20 -0300

    .

---
+++ pessoas_project/pessoas_webApi/pessoas_webApi/Controllers/PessoaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using pessoas_webApi.Domains;
+using pessoas_webApi.Interfaces;
+using pessoas_webApi.Repositories;
+
+namespace pessoas_webApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        private IPessoaRepository _pessoaRepository { get; set; }
+
+        public PessoaController()
+        {
+            _pessoaRepository = new PessoaRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pessoa> listaPessoas = _pessoaRepository.ReadAll();
+            return Ok(listaPessoas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Pessoa pessoa = _pessoaRepository.GetById(id);
+
+            if(pessoa == null)
+            {
+                return NotFound(new {
+                    mensagem = "Pessoa não encontrada",
+                });
+            }
+
+            try
+            {
+                return Ok(pessoa);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
+    }
+}
---
+++ pessoas_project/pessoas_webApi/pessoas_webApi/Repositories/PessoaRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pessoas_webApi.Contexts;
+using pessoas_webApi.Domains;
+using pessoas_webApi.Interfaces;
+
+namespace pessoas_webApi.Repositories
+{
+    public class PessoaRepository : IPessoaRepository
+    {
+        PessoaContext ctx = new PessoaContext();
+        public void Create(Pessoa pessoa)
+        {
+            ctx.Pessoas.Add(pessoa);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pessoa GetById(int id)
+        {
+            return ctx.Pessoas.Include(x => x.Emails).Include(x => x.Telefones).FirstOrDefault(x => x.IdPessoa == id);
+        }
+
+        public List<Pessoa> ReadAll()
+        {
+            return ctx.Pessoas.ToList();
+        }
+
+        public void Update(Pessoa pessoa, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
