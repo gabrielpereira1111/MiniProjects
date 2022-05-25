c61467d69eb1c486f048526cbbcef754ac3b2bd4
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-25 17:41:05 -0300

    Adiciona context

--- locadora_project/locadora_webApi/locadora_webApi/Context/LocadoraContext.cs
+++ locadora_project/locadora_webApi/locadora_webApi/Context/LocadoraContext.cs
@@ -1,6 +1,18 @@
-﻿namespace locadora_webApi.Context
+﻿using locadora_webApi.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace locadora_webApi.Context
 {
-    public class LocadoraContext
+    public class LocadoraContext : DbContext
     {
+        public DbSet<Alugueis> Alugueis { get; set; } = null!;
+        public DbSet<Clientes> Clientes { get; set; } = null!;
+        public DbSet<Empresas> Empresas { get; set; } = null!;
+        public DbSet<Marcas> Marcas { get; set; } = null!;
+        public DbSet<Modelos> Modelos { get; set; } = null!;
+        public DbSet<TiposUsuario> TiposUsuario { get; set; } = null!;
+        public DbSet<Usuarios> Usuarios { get; set; } = null!;
+        public DbSet<Veiculos> Veiculos { get; set; } = null!;
+
     }
 }
