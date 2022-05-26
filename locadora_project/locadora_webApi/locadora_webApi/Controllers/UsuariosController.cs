9cced7663990d6baeb73d8778dc29b3fd52c93b5
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-26 14:14:06 -0300

    Adiciona controller

---
+++ locadora_project/locadora_webApi/locadora_webApi/Controllers/UsuariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace locadora_webApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+    }
+}
