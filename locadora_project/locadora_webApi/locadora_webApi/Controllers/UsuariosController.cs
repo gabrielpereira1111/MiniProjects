38bb2d25e85c11778fd19d2c0e132eda89989eb2
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-26 15:08:23 -0300

    Adiciona a geração de um jwt no método de login

--- locadora_project/locadora_webApi/locadora_webApi/Controllers/UsuariosController.cs
+++ locadora_project/locadora_webApi/locadora_webApi/Controllers/UsuariosController.cs
@@ -3,6 +3,9 @@ using locadora_webApi.Interfaces;
 using locadora_webApi.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace locadora_webApi.Controllers
 {
@@ -23,7 +26,31 @@ namespace locadora_webApi.Controllers
             Usuarios usuarioBuscado = _usuariosRepository.Login(usuario.Email, usuario.Senha);
             if (usuarioBuscado != null)
             {
-                return Ok(usuarioBuscado);
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.idUsuario.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                    new Claim(ClaimTypes.Role, usuarioBuscado.IdTiposUsuario.ToString()),
+                    new Claim("Role", usuarioBuscado.IdTiposUsuario.ToString())
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chave-secreta-locadora"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var myToken = new JwtSecurityToken(
+                        issuer: "Minha API",
+                        audience: "Minha API",
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(30),
+                        signingCredentials : creds
+                    );
+
+                return Ok(
+                        new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(myToken)
+                        }
+                    );
             }
 
             return NotFound("Email e/ou senha incorretos!");
