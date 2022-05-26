9efe1df9fb90740bc1d40b2de1177783095f30c6
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-26 14:05:21 -0300

    Adiciona Interfaces de usuário

---
+++ locadora_project/locadora_webApi/locadora_webApi/Interfaces/IUsuariosRepository.cs
@@ -0,0 +1,9 @@
+﻿using locadora_webApi.Domains;
+
+namespace locadora_webApi.Interfaces
+{
+    public interface IUsuariosRepository
+    {
+        Usuarios Login(string email, string senha); 
+    }
+}
