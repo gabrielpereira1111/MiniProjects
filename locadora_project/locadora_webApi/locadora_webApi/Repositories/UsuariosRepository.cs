0c302da47a48953eec23d3ce9bb6e5ef3a51cfeb
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-26 14:07:30 -0300

    Adiciona Repository de usuários

---
+++ locadora_project/locadora_webApi/locadora_webApi/Repositories/UsuariosRepository.cs
@@ -0,0 +1,13 @@
+﻿using locadora_webApi.Domains;
+using locadora_webApi.Interfaces;
+
+namespace locadora_webApi.Repositories
+{
+    public class UsuariosRepository : IUsuariosRepository
+    {
+        public Usuarios Login(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
