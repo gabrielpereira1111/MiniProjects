2d4bb591c1f71c2026c2ce8a836dd9955e199b75
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-05-26 14:53:06 -0300

    Adiciona a validação do token

--- locadora_project/locadora_webApi/locadora_webApi/Program.cs
+++ locadora_project/locadora_webApi/locadora_webApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = "JwtBearer";
+                    options.DefaultChallengeScheme = "JwtBearer";
+                })
+                .AddJwtBearer("JwtBearer", options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = "Minha API",
+                        ValidAudience = "Minha API",
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chave-secreta-locadora")),
+                        ClockSkew = TimeSpan.FromMinutes(30)
+                    };
+                });
 
 var app = builder.Build();
 
@@ -17,6 +38,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
